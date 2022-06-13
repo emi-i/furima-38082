b73b40e221676aac2b74e1d3af127b3bd3b38336
emi-i <bacopatomato4@gmail.com>
2022-06-13 16:58:47 +0900

    itemsモデル　バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,14 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :defrayment_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :ship_date_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, presence: true
+  validates :image, presence: true
+
 end
