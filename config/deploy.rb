35ccfb5a2f23a5b71d1416b20c4a919f70d9b474
emi-i <bacopatomato4@gmail.com>
2022-06-21 18:41:55 +0900

    deploy.rb 修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock 3.17.0
+lock '3.17.0'
 
 # Capistranoのログの表示に利用する
 set :application, furima-38082
@@ -11,7 +11,7 @@ set :repo_url,  'git@github.com:Githubのemi-i/furima-38082.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, 2.6.5 #カリキュラム通りに進めた場合、’2.6.5’ です
+set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
